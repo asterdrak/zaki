c74595af2d0835b0c50e314c8817a7e2eb0f64bf
asterdrak <asterdrak@gmail.com>
2017-08-25 23:03:39 +0200

    adds pending specs

---
+++ spec/controllers/user_sessions_controller_spec.rb
@@ -0,0 +1,27 @@
+require 'rails_helper'
+
+RSpec.describe UserSessionsController, type: :controller do
+  describe 'GET #create' do
+    it 'returns http success' do
+      skip
+      # get :create
+      # expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe 'GET #failure' do
+    it 'returns http success' do
+      skip
+      # get :failure
+      # expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe 'GET #destroy' do
+    it 'returns http success' do
+      skip
+      # get :destroy
+      # expect(response).to have_http_status(:success)
+    end
+  end
+end
