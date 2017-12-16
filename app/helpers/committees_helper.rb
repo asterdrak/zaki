b276681abb3a4c574973fc31961c556a136e67b0
Wojciech Zakrzewski <asterdrak@gmail.com>
2017-12-16 18:41:34 +0100

    adds link to stateman for admin

--- app/helpers/committees_helper.rb
+++ app/helpers/committees_helper.rb
@@ -3,4 +3,8 @@ module CommitteesHelper
   def folders
     @drive.authorized.find_folders_by_name(params[:drive_root]).files.collect { |f| [f.name, f.id] }
   end
+
+  def stateman_url(commitee)
+    Rails.application.secrets.stateman_url + '/organizations/' + commitee.stateman.organization_id
+  end
 end
