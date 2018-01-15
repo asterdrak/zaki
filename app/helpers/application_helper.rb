1095217e9e7a223944bcfac179af83abcc7abf88
Wojciech Zakrzewski <asterdrak@gmail.com>
2018-01-15 22:38:54 +0100

    skip forcing permitted keyword when user is logged in
    very unprobable, that logged in user will need his permitted trials in
    formsub, in this case, he can select proper button in registration form

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -10,11 +10,11 @@ module ApplicationHelper
 
   # rubocop:disable LineLength
   def formsub_meetings_url
-    "#{Rails.application.secrets.formsub_url}/committees/#{@committee.formsub_committee_id}/meetings?force_permitted_keyword_get=true"
+    "#{Rails.application.secrets.formsub_url}/committees/#{@committee.formsub_committee_id}/meetings" + (current_user.present? ? '' : '?force_permitted_keyword_get=true')
   end
 
   def formsub_committee_url
-    "#{Rails.application.secrets.formsub_url}/committees/#{@committee.formsub_committee_id}?force_permitted_keyword_get=true"
+    "#{Rails.application.secrets.formsub_url}/committees/#{@committee.formsub_committee_id}" + (current_user.present? ? '' : '?force_permitted_keyword_get=true')
   end
   # rubocop:enable LineLength
 
